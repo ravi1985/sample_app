9bc44f619a38dc184d21861b1bb7390112011a22
Ravikumar <rkravikumar988@gmail.com>
2019-10-10 11:30:45 +0530

    Layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'users/new'
+  root 'static_pages#home'
+  match '/signup', to: 'users#new', via: 'get'
+  # get 'static_pages/home'
   get 'static_pages/help'
+  get "static_pages/about"
+  get "static_pages/contact"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
